6da0412c7518604f98a1117cde7d7b5745adb407
Giovanni Coia <g.coia@icloud.com>
2016-06-01 21:03:36 -0700

    adding openCV based motion detection script

---
+++ scripts/motion_camera_2/surveillance.py
@@ -0,0 +1,102 @@
+# USAGE
+# python pi_surveillance.py --conf conf.json
+
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+from motioncamera.tempimage import TempImage
+import argparse
+import warnings
+import datetime
+import imutils
+import json
+import time
+import cv2
+
+# Parse arguments from JSON config file
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--conf", required=True, help="path to the JSON configuration file")
+args = vars(ap.parse_args())
+
+warnings.filterwarnings("ignore")
+conf = json.load(open(args["conf"]))
+
+#intialise camera
+camera = PiCamera()
+camera.resolution = tuple(conf["resolution"])
+camera.framerate = conf["fps"]
+rawCapture = PiRGBArray(camera, size=camera.resolution)
+
+# allow the camera to warmup, then initialize the average frame, last
+# uploaded timestamp, and frame motion counter
+print("[INFO] warming up...")
+time.sleep(conf["camera_warmup_time"])
+avg = None
+lastUploaded = datetime.datetime.now()
+motionCounter = 0
+
+for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image and initialize
+    frame = f.array
+    timestamp = datetime.datetime.now()
+    motionDetected = False
+
+    # resize the frame, convert it to grayscale, and blur it
+    frame = imutils.resize(frame, width=500)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    # if the average frame is None, initialize it
+    if avg is None:
+        print("[INFO] starting background model...")
+        avg = gray.copy().astype("float")
+        rawCapture.truncate(0)
+        continue
+
+    # accumulate the weighted average between the current frame and
+    # previous frames, then compute the difference between the current
+    # frame and running average
+    cv2.accumulateWeighted(gray, avg, 0.5)
+    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+
+    # threshold the delta image, dilate the thresholded image to fill
+    # in holes, then find contours on thresholded image
+    thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    (_, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # loop over the contours
+    for c in cnts:
+        # if the contour is too small, ignore it
+        if cv2.contourArea(c) < conf["min_area"]:
+            continue
+
+        motionDetected = True
+
+    # draw the text and timestamp on the frame
+    ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
+    cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                0.35, (0, 0, 255), 1)
+
+    #Check to see if motion
+    if motionDetected:
+        # check to see if enough time has passed between uploads
+        if (timestamp - lastUploaded).seconds >= conf["min_upload_seconds"]:
+            # increment the motion counter
+            motionCounter += 1
+
+            # check to see if the number of frames with consistent motion is
+            # high enough
+            if motionCounter >= conf["min_motion_frames"]:
+                t = TempImage()
+                cv2.imwrite(t.path, frame)
+
+                # update the last uploaded timestamp and reset the motion
+                # counter
+                lastUploaded = timestamp
+                motionCounter = 0
+
+    else:
+        motionCounter = 0
+
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
